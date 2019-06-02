bc478bf72cfc365d5c3270b054df4fc62010e7da
DavidAMcIntosh <davidmcintoshbz95@gmail.com>
2019-06-02 14:41:50 -0600

    [feat] allow user to add app to home screen of device

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+let deferredPrompt;
+
 // check if the browser supports service workers
 if ('serviceWorker' in navigator) {
     // register the service worker
@@ -7,3 +9,12 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worker Registered!');
         });
 }
+
+// intercept the beforeinstallprompt event and prevent Chrome from automatically
+// prompting the user. Save the event for use later.
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+})
